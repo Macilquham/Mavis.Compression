f75d0619d569decfce1eba10a2b6aa1ccf73768d
Macilquham <kronfield@hotmail.com>
2026-03-12 21:50:05 +1300

    adding compression

---
+++ Compression.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Mavis.Compression
+{
+    public class Compression
+    {
+        public byte[] Compress(string input)
+        {
+
+            var chars = input.AsSpan();
+            Dictionary<char, int> characterCount = BuildCharacterCount(input);
+
+            return [0b1000111];
+        }
+
+        private Dictionary<char, int> BuildCharacterCount(string input)
+        {
+            var inputAsSpan = input.AsSpan();
+            Dictionary<char, int> characterCount = [];
+
+            foreach(var c in inputAsSpan) 
+            { 
+                characterCount[c] = characterCount.GetValueOrDefault(c, 0) + 1;
+            }
+
+            return characterCount;
+        }
+
+        public string Decompress(byte[] input)
+        {
+            return ASCIIEncoding.UTF8.GetString(input);
+        }
+    }
+}
---
+++ CompressionTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Mavis.Compression
+{
+    
+    public class CompressionTests
+    {
+        [Fact]
+        public void TestOne() {
+            var compression = new Compression();
+            var compressed = compression.Compress("foobar");
+        }
+    }
+}
